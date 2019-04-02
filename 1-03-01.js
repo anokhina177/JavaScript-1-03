bd24aec9b196cc60150730c48f40a7e450215a04
anokhina177 <anoshkina@list.ru>
2019-04-02 20:56:01 +0200

    ttt

--- 1-03-01.js
+++ 1-03-01.js
@@ -1,30 +1,45 @@
 // task #1
-let i=2;
-while (i<=100) {    
+let i = 2;
+while (i <= 100) {
     let isPrime = true;
-    let j=2;
-    while (j<i && isPrime) {
+    let j = 2;
+    while (j < i && isPrime) {
         if (i % j == 0) isPrime = false;
         j++;
     }
- 
+
     if (isPrime) console.log(i); // простое
 
     i++;
 }
 
 // task #2 & #3
-let basketItems = [
-    {product: 'Apples', quantity: 5, price: 100},
-    {product: 'Oranges', quantity: 7, price: 120},
-    {product: 'Peaches', quantity: 3, price: 140},
-    {product: 'Grapes', quantity: 2, price: 110}
+let basketItems = [{
+        product: 'Apples',
+        quantity: 5,
+        price: 100
+    },
+    {
+        product: 'Oranges',
+        quantity: 7,
+        price: 120
+    },
+    {
+        product: 'Peaches',
+        quantity: 3,
+        price: 140
+    },
+    {
+        product: 'Grapes',
+        quantity: 2,
+        price: 110
+    }
 ];
 
 function countBasketPrice(items) {
     let sumTotal = 0;
     for (let i = 0; i < items.length; i++) {
-        sumTotal += items[i].quantity * items[i].price; 
+        sumTotal += items[i].quantity * items[i].price;
     }
 
     return sumTotal;
@@ -35,10 +50,18 @@ console.log(countBasketPrice(basketItems));
 
 // task #4
 for (
-    var i = 0; 
-    i < 10; 
-    console.log(i), 
+    var i = 0; i < 10; console.log(i),
     i++
-){}
+) {}
 
 // task #5
+function printStars(count) {
+    let str = "";
+    for (let i = 0; i < count; i++)
+        str += "*";
+
+    console.log(str);
+}
+
+for (let i = 1; i <= 20; i++)
+    printStars(i);
